5965fd02ee346a2babfa78331b7bea4d60e41481
UnHingedFrog <84333538+UnHingedFrog@users.noreply.github.com>
2021-05-25 17:04:48 +0530

    Add files via upload

---
+++ hashtest/hashtest1.py
@@ -0,0 +1,21 @@
+import hashlib
+import timeit
+
+#crypt=input("")
+crypt="testblock"
+i=1 
+startTime=timeit.default_timer()
+while 1:
+    cryptt=crypt+str(i)
+    crypts=hashlib.sha256(cryptt.encode())
+    crypts=crypts.hexdigest()
+    if crypts[:6] == "000000" :
+        print(str(i)+"\n")
+        timediff=timeit.default_timer()-startTime
+        print("Time: " + str(timediff) + " seconds")
+        break
+    else :
+        if i%1000000 == 0 :
+            timediff=timeit.default_timer()-startTime
+            print("Time till " + str(i) + ": " + str(timediff) + " seconds")
+        i+=1
